f3ad614dd0ab0390a7cf8baed853a61d49dd37b5
Petar Repac <petarr.repac@gmail.com>
2024-05-11 02:00:08 +0200

    Lazyloading added

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,6 +134,25 @@
 
 
   /**
+   * Footer background lazy load
+   */
+  let footer = select('#footer');
+  let footerLazyLoaded = false;
+  if (footer) {
+    const lazyLoadFooter = () => {
+      if (window.scrollY > 400 && !footerLazyLoaded) {
+        footer.style.backgroundImage = 'url("assets/img/footer-1.webp")';
+        footer.style.backgroundPosition = 'center center';
+        footer.style.backgroundRepeat = 'no-repeat';
+        footerLazyLoaded = true;
+      }
+
+    }
+    window.addEventListener('load', lazyLoadFooter)
+    onscroll(document, lazyLoadFooter)
+  }
+
+  /**
    * Mobile nav toggle
    */
   on('click', '.contact-links-toggle', function(e) {
